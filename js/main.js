9404b162fae0248365ff59677dba94866836ba36
”OliviaAndersson” <olivia.moaandersson@gmail.com>
2021-11-02 19:39:55 +0100

    ToDo created and active/disabled mode were added

--- js/main.js
+++ js/main.js
@@ -11,8 +11,22 @@ window.onload = function(){
     document.getElementById("menu-option-three").addEventListener("click", closeMenuByOptionThree);
     document.getElementById("menu-option-four").addEventListener("click", closeMenuByOptionFour);
 
+    let addButton = document.getElementById("addButton");
+    addButton.addEventListener("click", createElementInToDo);
+
+    // let changeFromActive = document.getElementById("list-active");
+    // changeFromActive.addEventListener("click", changeFromActiveToDisabled);
+
+    // let changeFromDisabled = document.getElementById("list-disabled");
+    // changeFromDisabled = document.addEventListener("click", changeFromDisabledToActive);
+
 
 }
+
+let listNumber = 0;
+let toDoList = [];
+let numbersOfClick = 0;
+
 
 function createMenu(){
     document.getElementById("hamburger-wrapper").style.display = "block";
@@ -36,4 +50,44 @@ function closeMenuByOptionThree(){
 
 function closeMenuByOptionFour(){
     document.getElementById("hamburger-wrapper").style.display = "none";
+}
+
+function  createElementInToDo(){
+
+    if (addToList.value === ""){
+        alert("Du måste skriva in något i rutan!")
+    }
+    else{
+
+        let addToList = document.getElementById("addToList");
+        let ulList = document.getElementById("to-do-list");
+
+        toDoList.push(addToList.value);
+
+        let createElement = document.createElement("li"); 
+
+        for (let i = 0; i < toDoList.length; i++) {
+            createElement.id = "list-active";
+            createElement.innerHTML = addToList.value;
+            ulList.appendChild(createElement);        
+        }
+    
+        document.getElementById("addToList").value = "";
+        console.log(toDoList);
+
+        document.getElementById("list-active").addEventListener("click", changeFromActiveToDisabled);
+
+        function changeFromActiveToDisabled(ev){
+            console.log(ev);
+            if (ev.target.localName === "li") {
+                ev.target.classList.toggle("list-disabled");
+            }
+
+            else{}
+
+        }
+
+    }
+
+  
 }
